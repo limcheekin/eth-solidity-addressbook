0520b0b198118199bcdeab06b4fee6a44673fbca
Lim Chee Kin <limcheekin@vobject.com>
2023-02-27 17:29:20 +0800

    chore: updated hardhat config to use dotenv for deployment to goerli testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  },
+
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
+  },  
 };
 
 export default config;
