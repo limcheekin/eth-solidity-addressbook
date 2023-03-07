34839b54e8b2c546109fdeb0bbb3f36bec65ae2d
Lim Chee Kin <limcheekin@vobject.com>
2023-03-07 17:30:38 +0800

    chore: migrated to yarn and added gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,8 +6,10 @@ dotenv.config();
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
   solidity: "0.8.17",
 
   etherscan: {
@@ -19,7 +21,15 @@ const config: HardhatUserConfig = {
       url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [GOERLI_PRIVATE_KEY],
     },
-  },  
+  },
+  
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    currency: "USD",
+    noColors: true,
+    coinmarketcap: COINMARKETCAP_API_KEY,
+  }
 };
 
 export default config;
